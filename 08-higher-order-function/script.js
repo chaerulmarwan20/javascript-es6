92429d01a805baaa8b569cddf38212e1241f2c17
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2021-02-17 15:47:30 +0700

    mengedit folder 08 dan 19

---
+++ 08-higher-order-function/script.js
@@ -0,0 +1,31 @@
+// kerjakanTugas = Higher Order Function
+// selesai = Callback
+function kerjakanTugas(matakuliah, selesai) {
+	console.log(`Mulai mengerjakan tugas ${matakuliah} ...`);
+	selesai();
+}
+
+function selesai() {
+	// alert('Selesai mengerjakan tugas!');
+	console.log('Selesai mengerjakan tugas!');
+}
+
+kerjakanTugas('Pemrograman Web', selesai);
+
+
+// setTimeout = Higher Order Function
+// function = Callback
+setTimeout(function() {
+	console.log('Hello World');
+}, 1000);
+
+// ucapkanSalam = Higher Order Function
+function ucapkanSalam(waktu) {
+	// function(nama) = Return Value Berupa Function
+	return function(nama) {
+		console.log(`Halo ${nama}, Selamat ${waktu}, semoga harimu menyenangkan!`);
+	}
+}
+
+let selamatMalam = ucapkanSalam('Malam');
+console.dir(selamatMalam('Chaerul'));
